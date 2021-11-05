a55e424eacb60fa0653c5fd930d0740f44388f5d
natasha-reznikova <reznikova0586@gmail.com>
2021-11-05 21:12:05 +0200

    1е задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+const btnStart = document.querySelector('button[data-start]')
+console.log(btnStart)
+const btnStop = document.querySelector('button[data-stop]')
+console.log(btnStop)
+
+
+btnStart.addEventListener('click', onStartClick)
+btnStop.addEventListener('click', onStopClick);
+
+function onStartClick() {
+    btnStart.setAttribute("disabled", "disabled");
+    btnStop.removeAttribute("disabled")
+
+    setTimeout(() => {
+        timerId = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor()
+        }, 1000)
+    
+    });
+}
+function onStopClick() {
+    clearInterval(timerId);
+    console.log(`Stop`);
+    btnStop.setAttribute("disabled", "disabled")
+    btnStart.removeAttribute("disabled")
+};
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
