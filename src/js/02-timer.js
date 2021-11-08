e5640a12fada03389b3f64e867ec5170a4cf2659
natasha-reznikova <reznikova0586@gmail.com>
2021-11-08 22:45:39 +0200

    3е задание

---
+++ src/js/02-timer.js
@@ -0,0 +1,40 @@
+// import flatpickr from "flatpickr"
+// import 'flatpickr/dist/flatpickr.min.css';
+
+// const btnStart = document.querySelectorAll(button[data-start])
+// const datetimePicker = document.querySelector('#datetime-picker')
+
+
+// function flatpickr(selector, options)
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// function convertMs() {
+    
+// }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", evt => {
+  
+  evt.preventDefault();
+  
+const { delay, step, amount } = evt.currentTarget;
+   let firstDelay = Number(delay.value);
+  const stepDelay = Number(step.value);
+  const amountPromises = Number(amount.value);
+  for (let i = 1;  i <= amountPromises; i++) {
+    
+    createPromise(i, firstDelay);
+
+    console.log('delay', firstDelay, 'position', i);
+    firstDelay += stepDelay;
+    form.reset()  };
+
+});
 function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    setTimeout(() => {
+
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      };
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    });
 }
